2d99339f090e5f65836392deae4e5d1f9efa7164
Jesus Finol <jefinol@hiberus.com>
2025-12-02 10:50:36 +0100

    initial commit

---
+++ src/main/java/com/backandwhite/core/CoreBackandwhiteApplication.java
@@ -0,0 +1,13 @@
+package com.backandwhite.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBackandwhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBackandwhiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backandwhite/core/CoreBackandwhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backandwhite.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBackandwhiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
